f0acdcd9e03736c293fee963d3112d00f7f8b6ca
Bleyle <Bennybleylle09@gmail.com>
2022-02-15 14:37:34 +0300

    Add files via upload

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,28 +1,44 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity ^0.8.3;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFT is ERC721URIStorage {
+
+    //auto-increment field for each token
     using Counters for Counters.Counter;
+
     Counters.Counter private _tokenIds;
+
+    //address of the NFT market place
+    //https://t.me/techjobsng
+
     address contractAddress;
 
-    constructor(address marketplaceAddress) ERC721("Metaverse", "METT") {
-        contractAddress = marketplaceAddress;
+    constructor(address marketplaceAddress) ERC721("Partnerverse Tokens", "PNVT"){
+       contractAddress = marketplaceAddress;
     }
 
-    function createToken(string memory tokenURI) public returns (uint) {
+    /// @notice create a new token
+    /// @param tokenURI : token URI
+    function createToken(string memory tokenURI) public returns(uint) {
+        //set a new token id for the token to be minted
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
 
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        setApprovalForAll(contractAddress, true);
+        _mint(msg.sender, newItemId); //mint the token
+        _setTokenURI(newItemId, tokenURI); //generate the URI
+        setApprovalForAll(contractAddress, true); //grant transaction permission to marketplace
+
+        //return token ID
         return newItemId;
+
     }
+     function transferToken(address from, address to, uint256 tokenId) external {
+        require(ownerOf(tokenId) == from, "From address must be token owner");
+        _transfer(from, to, tokenId);
+    }
+
 }
